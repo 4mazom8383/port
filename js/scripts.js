1371fda1458594b08c36991c5cbdf0c6abd4951d
Mohamed Mazouz <72255167+4mazom8383@users.noreply.github.com>
2022-06-20 14:41:21 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+      $('.sidenav').sidenav({
+            edge: 'right',
+            draggable: 'true'
+      });
+
+      $('.modal').modal(); 
+      instance.close();
+      $('.materialboxed').materialbox();
+
+
+      /*=SLIDER=*/
+      $('.slider').slider();
+      /*=Nav=*/
+      $(".button-collapse").sideNav();
+ 
+
+
+
+
+
+ 
+});
